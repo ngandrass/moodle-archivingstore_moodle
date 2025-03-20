51b373f7556eb73273bd415069df60c833172a10
Niels Gandraß <niels@gandrass.de>
2025-03-20 15:46:21 +0100

    Implement basic plugin interface

---
+++ classes/archivingstore.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Driver for storing archive data inside via the Moodle File API.
+ *
+ * @package     archivingstore_moodle
+ * @copyright   2025 Niels Gandraß <niels@gandrass.de>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace archivingstore_moodle;
+
+use local_archiving\driver\archivingstore_base;
+
+// @codingStandardsIgnoreFile
+defined('MOODLE_INTERNAL') || die(); // @codeCoverageIgnore
+
+
+/**
+ * Driver for storing archive data inside via the Moodle File API.
+ */
+class archivingstore extends archivingstore_base {
+
+    /**
+     * @inheritDoc archivingstore_base::get_name()
+     */
+    public static function get_name(): string {
+        return get_string('pluginname', 'archivingstore_moodle');
+    }
+
+}
